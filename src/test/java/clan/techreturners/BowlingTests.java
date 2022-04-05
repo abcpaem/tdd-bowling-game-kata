d27ade2e3ee3d351aff8d37b891da4eb39e537d7
abcpaem <abcpaem@gmail.com>
2022-04-05 15:40:06 +0100

    Adds test for checking the final score in an open frames game with same knocks per frame

--- src/test/java/clan/techreturners/BowlingTests.java
+++ src/test/java/clan/techreturners/BowlingTests.java
@@ -1,18 +1,15 @@
 package clan.techreturners;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingTests {
+    private final int TOTAL_FRAMES = 10;
+
     @ParameterizedTest(name = "{index}) Two knocks of {0} and {1} will produce a score of {2}")
-    @CsvSource(textBlock = """
-            3,4, 7
-            2,2, 4
-            6,3, 9
-            """)
+    @CsvSource({"3,4,7", "2,2,4", "6,3,9"})
     public void checkScoreForOpenFramesGameWithOneFrame(int firstThrow, int secondThrow, int expectedScore) {
         // Arrange
         Bowling game = new Bowling();
@@ -24,5 +21,22 @@ public class BowlingTests {
 
         // Assert
         assertEquals(expectedScore, score);
+    }
+
+    @ParameterizedTest(name = "A game with 10 frames of two knocks of {0} and {1} will produce a final score of {2}")
+    @CsvSource({"5,2,70", "9,0,90", "2,4,60"})
+    public void checkFinalScoreForOpenFramesGameWithSameKnocksPerFrame(int firstThrow, int secondThrow, int expectedFinalScore) {
+        // Arrange
+        Bowling game = new Bowling();
+
+        // Act
+        for (int frames = 1; frames <= TOTAL_FRAMES; frames++) {
+            game.knock(firstThrow);
+            game.knock(secondThrow);
+        }
+        int score = game.getScore();
+
+        // Assert
+        assertEquals(expectedFinalScore, score);
     }
 }
