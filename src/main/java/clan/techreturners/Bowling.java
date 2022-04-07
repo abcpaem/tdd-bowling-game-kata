024563c8b537616ef4f43cc2f96a9f7328d6f81e
abcpaem <abcpaem@gmail.com>
2022-04-08 00:02:17 +0100

    Add tests for a game with mixed frames

--- src/main/java/clan/techreturners/Bowling.java
+++ src/main/java/clan/techreturners/Bowling.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Bowling {
     private final Integer MAX_FRAMES = 10;
+    private final Integer STRIKE = 10;
     private ArrayList<Frame> frames = new ArrayList<>();
     private Integer currentFrameIndex = 0;
 
@@ -24,16 +25,21 @@ public class Bowling {
 
     public int getScore() {
         updateBonusPoints();
-        return frames.stream().limit(MAX_FRAMES).mapToInt(f -> f.getScore()).sum();
+        return frames.stream().limit(MAX_FRAMES).mapToInt(Frame::getScore).sum();
     }
 
     private void updateBonusPoints() {
+        frames.forEach(f -> f.setBonusPoints(0));
         for (int i = 0; i < frames.size(); i++) {
             Frame frame = frames.get(i);
             if (frame.getType() == Frame.FrameType.SPARE && i + 1 < frames.size()) {
                 frame.setBonusPoints(frames.get(i + 1).getFirstKnock());
             } else if (frame.getType() == Frame.FrameType.STRIKE && i + 2 < frames.size()) {
-                frame.setBonusPoints(frames.get(i + 1).getScore() + frames.get(i + 2).getScore());
+                Frame nextFrame = frames.get(i + 1);
+                int bonus = nextFrame.getFirstKnock() == STRIKE
+                        ? nextFrame.getFirstKnock() + frames.get(i + 2).getFirstKnock()
+                        : nextFrame.getScore();
+                frame.setBonusPoints(bonus);
             }
         }
     }
--- src/test/java/clan/techreturners/BowlingTests.java
+++ src/test/java/clan/techreturners/BowlingTests.java
@@ -1,5 +1,6 @@
 package clan.techreturners;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,18 +11,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingTests {
     private final Integer STRIKE = 10;
-    private Bowling game = new Bowling();
+    private Bowling game;
 
-    @ParameterizedTest(name = "{index}) A game with {3} throws in frames of {0}, {1}, will produce a score of {2}")
+    @BeforeEach
+    void Init() {
+        game = new Bowling();
+    }
+
+    @ParameterizedTest(name = "{index}) A game with frames {0}, will produce a score of {1}")
     @MethodSource
-    void checkScoreForBowlingGameInFrames(int pinsFirstThrow, int pinsSecondThrow, int expectedScore, int totalThrows) {
-        for (int i = 1; i <= totalThrows; i++) {
-            game.knock(i % 2 == 1 || pinsFirstThrow == STRIKE ? pinsFirstThrow : pinsSecondThrow);
+    void checkScoreForBowlingGameWithMixedFrames(String[] frames, int expectedScore) {
+        for (String frame : frames) {
+            String[] pins = frame.split(",");
+            int pinsFirstThrow = Integer.parseInt(pins[0]);
+            roll(pinsFirstThrow, Integer.parseInt(pins[1]), pinsFirstThrow == STRIKE ? 1 : 2);
         }
         assertEquals(expectedScore, game.getScore());
     }
 
-    public static Stream<Arguments> checkScoreForBowlingGameInFrames() {
+    @ParameterizedTest(name = "{index}) A game with {3} throws in frames of {0}, {1}, will produce a score of {2}")
+    @MethodSource
+    void checkScoreForBowlingGameWithFixedFrames(int pinsFirstThrow, int pinsSecondThrow, int expectedScore, int totalThrows) {
+        roll(pinsFirstThrow, pinsSecondThrow, totalThrows);
+        assertEquals(expectedScore, game.getScore());
+    }
+
+    private void roll(int pinsFirstThrow, int pinsSecondThrow, int totalThrows) {
+        for (int i = 1; i <= totalThrows; i++) {
+            game.knock(i % 2 == 1 || pinsFirstThrow == STRIKE ? pinsFirstThrow : pinsSecondThrow);
+        }
+    }
+
+    public static Stream<Arguments> checkScoreForBowlingGameWithFixedFrames() {
         return Stream.of(
                 // One frame
                 Arguments.of(3, 4, 7, 2),
@@ -37,6 +58,14 @@ public class BowlingTests {
                 Arguments.of(9, 1, 190, 21),
                 // Strike Frames
                 Arguments.of(10, 0, 300, 12)
+        );
+    }
+
+    public static Stream<Arguments> checkScoreForBowlingGameWithMixedFrames() {
+        return Stream.of(
+                Arguments.of(new String[]{"5,2", "6,4", "1,0", "10,0", "7,3", "10,0", "10,0", "0,4", "5,5", "3,3"}, 116),
+                Arguments.of(new String[]{"10,0", "8,0", "5,5", "10,0", "1,0", "10,0", "9,1", "0,0", "10,0", "3,7", "10,0"}, 128),
+                Arguments.of(new String[]{"1,1", "10,0", "10,0", "10,0", "4,6", "2,8", "10,0", "6,0", "2,0", "10,0", "10,0", "5,0"}, 157)
         );
     }
 }
